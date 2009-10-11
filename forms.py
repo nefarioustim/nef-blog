be83763822cddbce9769cb76b1134ec1e95e7ae9
Tim Huegdon <tim@nefariousdesigns.co.uk>
2009-10-11 23:06:30 +0100

    Contact form changes.

--- forms.py
+++ forms.py
@@ -1,7 +1,10 @@
+# -*- coding: utf-8 -*-
+
 from django import forms
 
 class ContactForm(forms.Form):
+    sender = forms.EmailField(label='Your name')
     subject = forms.CharField(max_length=100)
-    message = forms.CharField()
-    sender = forms.EmailField()
-    cc_myself = forms.BooleanField(required=False)
+    message = forms.CharField(widget=forms.Textarea)
+    cc_myself = forms.BooleanField(label=u'“I would like to receive a copy of \
+    the email myself.”', required=False)
--- templatetags/blog_tags.py
+++ templatetags/blog_tags.py
@@ -1,6 +1,9 @@
 from django import template
 from blog.models import Post
 
+register = template.Library()
+
+@register.tag(name="get_latest_posts")
 def do_latest_posts(parser, token):
     return LatestPostsNode()
 
@@ -8,6 +11,3 @@ class LatestPostsNode(template.Node):
     def render(self, context):
         context['latest_posts'] = Post.pub.all()[:3]
         return ''
-
-register = template.Library()
-register.tag('get_latest_posts', do_latest_posts)
